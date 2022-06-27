4ebe838de5084c453aa25f4b18f0fca43304d1f0
romero8 <101278601+romero8@users.noreply.github.com>
2022-06-27 14:40:12 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Category from './components/Category'
+
+function App() {
+  return (
+    <div className="App">
+      <Category></Category>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Categories.js
@@ -0,0 +1,125 @@
+const Categories =[
+    {
+        
+        id:1,
+        title:"Rose Sympathy",
+        price:300,
+        category:"BEST SELLERS",
+        image:"./images/flowers/rose-flowers-bouquet-isolated-27274345_gircwg.jpg"
+    },
+
+    {
+        id:2,
+        title:"Beautiful Big Bouquet Of Many Flowers",
+        price:200,
+        category:"SPECIALS",
+        image:"./images/flowers/beautiful-big-bouquet-of-many-flowers-picture-id155351575_pflupy.jpg"
+    },
+
+    {
+        id:3,
+        title:"Blush and Gold Bouquet",
+        price:300,
+        category:"NEW ARRIVALS",
+        image:"./images/flowers/blush-and-gold-bouquet_qzqlc8.jpg"
+    },
+
+    {
+        id:4,
+        title:"Carnations-Lilies",
+        price:300,
+        category:"NEW ARRIVALS",
+        image:"./images/flowers/carnations-lilies-hand-tied-bunch_1_ifzafm.jpg"
+    },
+
+    {
+        id:5,
+        title:"Harry-David-Lily",
+        price:300,
+        category:"BEST SELLERS",
+        image:"./images/flowers/harry-david-lily-bouquet_kjy7ld.jpg"
+    },
+
+    {
+        id:6,
+        title:"Purple And Pink",
+        price:300,
+        category:"BEST SELLERS",
+        image:"./images/flowers/image_ckdxxs.jpg"
+    },
+
+    {
+        id:7,
+        title:"Orange And Yellow",
+        price:300,
+        category:"NEW ARRIVALS",
+        image:"./images/flowers/IT15-bouquet-of-orange-and-yellow-flowers.jpg-500x500-1_smjdes.jpg"
+    },
+
+    {
+        id:8,
+        title:"Perfect Love Bouquet",
+        price:300,
+        category:"SPECIALS",
+        image:"./images/flowers/perfect-love-bouquet-fresh-flowers-VA00707.425_swcovb.jpg"
+    },
+
+    {
+        id:9,
+        title:"Premixed",
+        price:300,
+        category:"SPECIALS",
+        image:"./images/flowers/premixed-bouquet-flower-0e49a64c_rilgcm.jpg"
+    },
+
+    {
+        id:10,
+        title:"Roses",
+        price:300,
+        category:"NEW ARRIVALS",
+        image:"./images/flowers/xred-rose-sympathy-flowers.jpg.pagespeed.ic.6KjqrCJdm9_lid3jf.jpg"
+    },
+
+    {
+        id:11,
+        title:"Special Roses",
+        price:300,
+        category:"BEST SELLERS",
+        image:"./images/flowers/rose-flowers-bouquet-isolated-28428162_rzl96h.jpg"
+    },
+
+    {
+        id:12,
+        title:"Orange",
+        price:300,
+        category:"BEST SELLERS",
+        image:"./images/flowers/Shopping-Bouqs-Flowers_itd9nu.jpg"
+    },
+
+    {
+        id:13,
+        title:"Bouqs Flowers",
+        price:300,
+        category:"NEW ARRIVALS",
+        image:"./images/flowers/Shopping-FTD-Flowers_ums3ng.jpg"
+    },
+
+    {
+        id:14,
+        title:"Silence Roses",
+        price:300,
+        category:"SPECIALS",
+        image:"./images/flowers/slide_16_ikfz5y.jpg"
+    },
+
+    {
+        id:15,
+        title:"Sympathy Bouquet Of Flowers",
+        price:300,
+        category:"SPECIALS",
+        image:"./images/flowers/xColourful-sympathy-bouquet-flowers-fast.jpg.pagespeed.ic.eCrDGv_4k3_neizyg.jpg"
+    },
+
+    
+]
+export default Categories
